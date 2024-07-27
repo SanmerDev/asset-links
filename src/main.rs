58f9903fad1cf7ec457132d09b46cb4124ae5819
SanmerDev <sanmer@sanmer.dev>
2024-07-27 15:01:31 +0800

    Fix `Content-Type`

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use std::fs;
 
-use actix_web::{App, get, HttpResponse, HttpServer, Responder};
-use actix_web::http::StatusCode;
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
 const ASSET_JSON: &str = "/etc/asset-links/asset.json";
 
 #[get("/assetlinks.json")]
 async fn assetlinks() -> impl Responder {
     let content = fs::read_to_string(ASSET_JSON).unwrap_or("[]".to_owned());
-    HttpResponse::with_body(StatusCode::OK, content)
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body(content)
 }
 
 #[actix_web::main]
